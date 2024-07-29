8ac58d5b871828c39be5670bd8089e9333908af0
Jansen Smith <jansen@bstarengineering.com>
2024-07-29 10:23:46 -0400

    Tmp save during fork

--- TechLogoPainting.groovy
+++ TechLogoPainting.groovy
@@ -2,7 +2,7 @@
 
 // import logo STL
 File logoSTL = ScriptingEngine.fileFromGit(
-	"https://github.com/JansenSmith/Technocopia_logo_chalk_Front_200x242_JansenSmith.git",
+	"https://github.com/JansenSmith/Technocopia_logo_chalk_painting.git",
 	"Technocopia_logo_chalk_Front_200x242.stl");
 // Load the .CSG from the disk and cache it in memory
 CSG logo  = Vitamins.get(logoSTL);
